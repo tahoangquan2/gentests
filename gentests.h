a7bc666f3682894b88f976149d51d34d8b40941b
Quan Hoang Ta <hoangquandt69@gmail.com>
2025-03-15 08:26:29 +0700

    RNG with cache

--- gentests.h
+++ gentests.h
@@ -1,8 +1,24 @@
-#include "time.h"
-#include "stdlib.h"
+#include <random>
+#include <time.h>
+#include <stdlib.h>
 
-const unsigned long long default_seed = (unsigned long long)(time(NULL));
+std::random_device rd;
+std::mt19937_64 gen;
+std::uniform_int_distribution<long long> rng(0, 1);
 
-void _set_seed(unsigned long long seed = default_seed) {
-  srand(seed);
+long long cache_l = 0, cache_r = 1;
+
+void _set_seed() {
+  gen.seed(rd());
+  cache_l = 0;
+  cache_r = 1;
+}
+
+long long _get_range(long long l, long long r) {
+  if (cache_l != l || cache_r != r) {
+    cache_l = l;
+    cache_r = r;
+    rng = std::uniform_int_distribution<long long>(l, r);
+  }
+  return rng(gen);
 }
--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
+#include <iostream>
 #include "gentests.h"
-#include "bits/stdc++.h"
-using namespace std;
+
 typedef long long ll;
-typedef pair<ll, ll> pa;
-
-#define forr(_a, _b, _c) for(_a = (_b); _a <= (_c); ++_a)
-#define ford(_a, _b, _c) for(_a = (_b) + 1; _a --> (_c);)
-#define forf(_a, _b, _c) for(_a = (_b); _a < (_c); ++_a)
-#define mp make_pair
-#define fi first
-#define se second
-#define vi vector
-#define sz(_v) _v.begin(), _v.end()
-#define mask(_x) (1ll << (_x))
-#define bit(_x, _y) (((_x) >> (_y)) & 1)
-
-const ll mod = ll(1e9) + 7;
-const int N = 2e5 + 5;
-int n, i, a[N];
 
 int main() {
+  clock_t tStart = clock();
+  _set_seed();
 
-  cin >> n;
+  ll l = 1, r = 1e18, x;
+  for (int i = 1; i <= 100000000; ++i) {
+    x = _get_range(l, r);
+  }
+  std::cout << x << "\n";
 
+  std::cout << (double)(clock() - tStart) / CLOCKS_PER_SEC;
   return 0;
 }
